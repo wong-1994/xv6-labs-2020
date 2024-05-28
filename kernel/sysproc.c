3c229055f7de0da2c35279934d2e5d06843cadfe
1994 wong <seanwxj@tongji.edu.cn>
2024-05-28 17:43:27 +0800

    lazy lab: finish

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,9 +46,16 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+
+  struct proc *p = myproc();
+  addr = p->sz;
+  // if(growproc(n) < 0)
+  //   return -1;
+  if (n < 0) {
+    p->sz = uvmdealloc(p->pagetable, p->sz, p->sz + n);
+  } else {
+    p->sz += n;
+  }
   return addr;
 }
 
--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,8 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "spinlock.h"
+#include "proc.h"
 
 /*
  * the kernel's page table.
@@ -101,6 +103,13 @@ walkaddr(pagetable_t pagetable, uint64 va)
     return 0;
 
   pte = walk(pagetable, va, 0);
+  if(pte == 0 || (*pte & PTE_V) == 0) {
+    if (uvmlazyalloc(va, myproc()) != 0) {
+      return 0;
+    } else {
+      pte = walk(pagetable, va, 0);
+    }
+  }
   if(pte == 0)
     return 0;
   if((*pte & PTE_V) == 0)
@@ -181,9 +190,11 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+      continue;
+      // panic("uvmunmap: walk");
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+      continue;
+      // panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -251,6 +262,29 @@ uvmalloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz)
   return newsz;
 }
 
+// lazy alloc implement
+int uvmlazyalloc(uint64 va, struct proc *p)
+{
+  char *mem;
+
+  va = PGROUNDDOWN(va);
+  if (va >= p->sz || va < p->trapframe->sp) {
+    return -1;
+  }
+  mem = kalloc();
+  if(mem == 0){
+    // uvmdealloc(pagetable, a, oldsz);
+    return -1;
+  }
+  memset(mem, 0, PGSIZE);
+  if(mappages(p->pagetable, va, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+    kfree(mem);
+    // uvmdealloc(pagetable, a, oldsz);
+    return -1;
+  }
+  return 0;
+}
+
 // Deallocate user pages to bring the process size from oldsz to
 // newsz.  oldsz and newsz need not be page-aligned, nor does newsz
 // need to be less than oldsz.  oldsz can be larger than the actual
@@ -315,9 +349,11 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+      continue;
+      // panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+      continue;
+      // panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
