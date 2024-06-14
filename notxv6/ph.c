d9714a9f809bb9107ee0f984c09d054aa8ccd8c7
1994 wong <seanwxj@tongji.edu.cn>
2024-06-14 23:23:15 +0800

    finish lab multithreading

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t bucketlocks[NBUCKET];
 
 double
 now()
@@ -51,7 +52,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&bucketlocks[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&bucketlocks[i]);
   }
 }
 
@@ -113,6 +116,9 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+  for (int i = 0; i < nthread; i++) {
+    pthread_mutex_init(&bucketlocks[i], NULL);
   }
 
   //
