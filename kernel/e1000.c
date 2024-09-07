2c60073e99c6e185dabe4c98912eb00d7e4d9ab4
1994 wong <seanwxj@tongji.edu.cn>
2024-09-07 17:17:46 +0800

    Finish Lab Networking

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -102,7 +102,33 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
+  if(m->len > MBUF_SIZE)
+    return -1;
+
+  acquire(&e1000_lock);
+
+  uint idx = regs[E1000_TDT];
+
+  if((tx_ring[idx].status & E1000_TXD_STAT_DD) == 0){
+    release(&e1000_lock);
+    return -1;
+  }
+
+  if (tx_mbufs[idx])
+    mbuffree(tx_mbufs[idx]);
   
+  tx_ring[idx].addr = (uint64) m->head;
+  tx_ring[idx].length = m->len;
+  tx_ring[idx].cmd = (E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP);
+  tx_ring[idx].status = 0;
+  tx_mbufs[idx] = m;
+
+  __sync_synchronize();
+
+  regs[E1000_TDT] = (idx + 1) % TX_RING_SIZE;
+
+  release(&e1000_lock);
+
   return 0;
 }
 
@@ -115,6 +141,31 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+  while(1){
+    uint idx = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+    if((rx_ring[idx].status & E1000_RXD_STAT_DD) == 0)
+      return;
+
+    __sync_synchronize();
+
+    if(!rx_mbufs[idx])
+      panic("e1000_recv: null ptr error.");
+    rx_mbufs[idx]->len = rx_ring[idx].length;
+
+    net_rx(rx_mbufs[idx]);
+    if(rx_mbufs[idx])
+      mbuffree(rx_mbufs[idx]);
+
+    rx_mbufs[idx] = mbufalloc(0);
+    if (!rx_mbufs[idx])
+      panic("e1000_recv: fail to alloc mbuf.");
+    rx_ring[idx].addr = (uint64) rx_mbufs[idx]->head;
+    rx_ring[idx].status = 0;
+
+    __sync_synchronize();
+
+    regs[E1000_RDT] = idx;
+  }
 }
 
 void
